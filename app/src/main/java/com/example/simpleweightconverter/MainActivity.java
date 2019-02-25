ef4832979783dd1215b18095b2efd0c3738a3d51
JT <jayt24seven@gmail.com>
2019-02-25 19:12:18 +1300

    - Added a few UI changes - Fixed a bug where if the user didn't input a value the app would crash due to a parsing to float issue - Added error messages for when the user doesn't input a value or the value is less than zero - Added a message for when the user switches between imperial and metric systems

--- app/src/main/java/com/example/simpleweightconverter/MainActivity.java
+++ app/src/main/java/com/example/simpleweightconverter/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.simpleweightconverter;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CompoundButton;
@@ -30,12 +32,20 @@ public class MainActivity extends AppCompatActivity {
         aswitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-
+                TextView currentSystem = (TextView) findViewById(R.id.textView3);
+                Toast toast;
+                // displays whether the user is using the metric or imperial system of measuring
                 if (isChecked == true){
-                    Toast.makeText(getBaseContext(),"Imperial",Toast.LENGTH_SHORT).show();
+                    toast = Toast.makeText(getBaseContext(),"Imperial",Toast.LENGTH_SHORT);
+                    toast.setGravity(Gravity.CENTER_VERTICAL, 0, 100); // set the position of the error message to the middle of the screen
+                    toast.show();
+                    currentSystem.setText("Imperial to Metric");
 
                 } else {
-                    Toast.makeText(getBaseContext(),"Metric", Toast.LENGTH_SHORT).show();
+                    toast = Toast.makeText(getBaseContext(),"Metric",Toast.LENGTH_SHORT);
+                    toast.setGravity(Gravity.CENTER_VERTICAL, 0, 100); // set the position of the error message to the middle of the screen
+                    toast.show();
+                    currentSystem.setText("Metric to Imperial");
 
                 }
             }
@@ -46,17 +56,32 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 EditText editText = (EditText) findViewById(R.id.editText);
                 TextView textView = (TextView) findViewById(R.id.textView);
-                double valueInput = Float.parseFloat(editText.getText().toString());
+                String valueInput = editText.getText().toString();
                 double weight = 0;
+                Toast errorMessage = Toast.makeText(getBaseContext(),"Please enter a value greater than zero", Toast.LENGTH_SHORT);
+                errorMessage.setGravity(Gravity.CENTER_VERTICAL, 0, 100); // set the position of the error message to the middle of the screen
+
+                if (TextUtils.isEmpty(valueInput)){ // displays an error message when input value is null
+                    errorMessage.show();
+                    return;
+
+                } else { // parse the input value to a float
+                    weight = Float.parseFloat(valueInput); //crash
+                }
+
+                if (weight <= 0) { // displays error message when input value is less than zero
+                    errorMessage.show();
+                    return;
+                }
 
                 if (aswitch.isChecked() == true){ // convert lb to kg
-                    weight = valueInput / 2.20462;
+                    weight = weight / 2.20462;
                     weight = Math.round(weight * 100) / 100.00; // rounding the value to two decimal places
                     String result = Double.toString(weight);
                     textView.setText(result + " kg");
 
                 } else {
-                    weight = valueInput * 2.20462; // convert kg to lb
+                    weight = weight * 2.20462; // convert kg to lb
                     weight = Math.round(weight * 100) / 100.00; // rounding the value to two decimal places
                     String result = Double.toString(weight);
                     textView.setText(result + " lb");
